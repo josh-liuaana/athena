1709f66683b96681cc62ff06a60fee5de7395417
Josh Liuaana <jfliuaana@gmail.com>
2023-12-21 17:48:27 +0000

    type file for interfacing

---
+++ models/types.ts
@@ -0,0 +1,20 @@
+export interface Person {
+  affiliations: string[]
+  aliases: string[]
+  books: string[]
+  city: string
+  ethnicity: string
+  id: string
+  name: string
+  relationships: Record<string, string>
+  universe: string
+}
+
+export interface Location {
+  id: string
+  location: string
+  nation: string
+  region: string
+  universe: string
+  world: string
+}
