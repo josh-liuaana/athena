d94a8d73950b78b4b568c17c37f575cba4113425
Josh Liuaana <jfliuaana@gmail.com>
2024-01-10 14:52:37 +0000

    remove unused code

--- models/data-functions.ts
+++
@@ -1,7 +0,0 @@
-export function capitalise(string) {
-  const words = string.split(' ')
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].substr(1)
-  }
-  return words.join(' ')
-}
