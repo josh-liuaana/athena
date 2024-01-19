ee596ac663d4d75ae5c45f0fa59b41ca4cd16c6c
Josh Liuaana <jfliuaana@gmail.com>
2024-01-19 22:38:04 +0000

    Wrap App in redux Provider

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useEffect, useState } from 'react'
 import { User, onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebaseConfig'
+import { Provider } from 'react-redux'
+
+import { store } from './redux/store'
 
 import Home from './components/Home'
 import Lore from './components/Lore'
@@ -37,94 +40,96 @@ export default function App() {
   if (!fontsLoaded) return null
 
   return (
-    <NavigationContainer>
-      {user ? (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              title: 'Home',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Lore"
-            component={Lore}
-            options={{
-              title: 'Lore',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Tomes"
-            component={Tomes}
-            options={{
-              title: 'Tomes',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Books"
-            component={Books}
-            options={{
-              title: 'Books',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="People"
-            component={People}
-            options={{
-              title: 'People',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Places"
-            component={Places}
-            options={{
-              title: 'Places',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="AddBook"
-            component={AddBook}
-            options={{
-              title: 'AddBook',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="AddCharacter"
-            component={AddCharacter}
-            options={{
-              title: 'AddCharacter',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="SingleCharacter"
-            component={SingleCharacter}
-            options={{
-              title: 'SingleCharacter',
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Logged out"
-            component={Login}
-            options={{
-              title: 'User logged out - login form',
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        {user ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                title: 'Home',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Lore"
+              component={Lore}
+              options={{
+                title: 'Lore',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Tomes"
+              component={Tomes}
+              options={{
+                title: 'Tomes',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Books"
+              component={Books}
+              options={{
+                title: 'Books',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="People"
+              component={People}
+              options={{
+                title: 'People',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Places"
+              component={Places}
+              options={{
+                title: 'Places',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="AddBook"
+              component={AddBook}
+              options={{
+                title: 'AddBook',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="AddCharacter"
+              component={AddCharacter}
+              options={{
+                title: 'AddCharacter',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="SingleCharacter"
+              component={SingleCharacter}
+              options={{
+                title: 'SingleCharacter',
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Logged out"
+              component={Login}
+              options={{
+                title: 'User logged out - login form',
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </Provider>
   )
 }
