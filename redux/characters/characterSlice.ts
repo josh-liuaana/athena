19e2f7768876441d63f9f7ec815a16daccb0d55c
Josh Liuaana <jfliuaana@gmail.com>
2024-01-24 16:13:06 +0000

    update character slice, store shape, fetch single character and update char

--- redux/characters/characterSlice.ts
+++ redux/characters/characterSlice.ts
@@ -2,25 +2,54 @@ import { createSlice } from '@reduxjs/toolkit'
 import {
   deleteCharacter,
   fetchCharacters,
+  fetchSingleCharacter,
   postCharacter,
+  updateCharacter,
 } from '../../apis/characters'
 
 import { Person } from '../../models/types'
 
-const initialState = [] as Person[]
+const initialState = {
+  characterList: [] as Person[],
+  activeCharacter: {} as Person,
+}
 
 export const characterSlice = createSlice({
   name: 'characters',
   initialState,
   reducers: {
     setCharacters: (state, action) => {
-      return action.payload
+      return {
+        characterList: action.payload,
+        activeCharacter: state.activeCharacter,
+      }
+    },
+    setActiveCharacter: (state, action) => {
+      return {
+        characterList: state.characterList,
+        activeCharacter: action.payload,
+      }
     },
     addCharacter: (state, action) => {
-      return [...state, action.payload]
+      return {
+        characterList: [...state.characterList, action.payload],
+        activeCharacter: state.activeCharacter,
+      }
     },
     delCharacter: (state, action) => {
-      return state.filter((character) => character.id !== action.payload.id)
+      const newCharList = state.characterList.filter(
+        (character) => character.id !== action.payload.id
+      )
+      return {
+        characterList: newCharList,
+        activeCharacter: state.activeCharacter,
+      }
+    },
+    updCharacter: (state, action) => {
+      return {
+        characterList: state.characterList,
+        activeCharacter: action.payload,
+      }
     },
   },
 })
@@ -29,6 +58,14 @@ export function fetchThunkCharacters() {
   return async (dispatch) => {
     const res = await fetchCharacters()
     dispatch(setCharacters(res))
+  }
+}
+
+export function fetchThunkSingleCharacter(id) {
+  return async (dispatch) => {
+    const res = await fetchSingleCharacter(id)
+    dispatch(setActiveCharacter(res))
+    //
   }
 }
 
@@ -46,6 +83,22 @@ export function deleteThunkCharacter(id) {
   }
 }
 
-export const { setCharacters, addCharacter, delCharacter } =
-  characterSlice.actions
+export function updateThunkCharacter(newCharacterInfo, id) {
+  return async (dispatch) => {
+    try {
+      const updatedCharacter = await updateCharacter(newCharacterInfo, id)
+      dispatch(updCharacter(updatedCharacter))
+    } catch (err) {
+      console.error('error inside thunk call:', err.message)
+    }
+  }
+}
+
+export const {
+  setCharacters,
+  addCharacter,
+  delCharacter,
+  updCharacter,
+  setActiveCharacter,
+} = characterSlice.actions
 export default characterSlice.reducer
