8ad57b9a609487b0043085b6cf7a98d2f8b9d45e
Josh Liuaana <jfliuaana@gmail.com>
2024-01-29 17:36:49 +0000

    Import and setup react native paper

--- App.tsx
+++ App.tsx
@@ -25,6 +25,7 @@ import Information from './components/Information'
 import { auth } from './firebase.config'
 
 import { store } from './redux/store'
+import { PaperProvider } from 'react-native-paper'
 
 const TomesStack = createNativeStackNavigator()
 
@@ -118,82 +119,84 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        {user ? (
-          <Tab.Navigator
-            initialRouteName="Home"
-            backBehavior="history"
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color }) => {
-                let iconName
+      <PaperProvider>
+        <NavigationContainer>
+          {user ? (
+            <Tab.Navigator
+              initialRouteName="Home"
+              backBehavior="history"
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color }) => {
+                  let iconName
 
-                if (route.name === 'Home') {
-                  iconName = focused ? 'home' : 'home-outline'
-                } else if (route.name === 'Tomes') {
-                  iconName = focused ? 'book' : 'book-outline'
-                } else if (route.name === 'User') {
-                  iconName = focused ? 'person' : 'person-outline'
-                } else if (route.name === 'Information') {
-                  iconName = focused
-                    ? 'information-circle'
-                    : 'information-circle-outline'
-                } else if (route.name === 'Lore') {
-                  iconName = focused ? 'add-circle' : 'add'
-                }
-                return <Ionicons name={iconName} size={30} color={color} /> // consider icon size
-              },
-              tabBarActiveTintColor: '#171D0B',
-              tabBarInactiveTintColor: 'white',
-              tabBarStyle: styles.bottomTabStyle,
-              tabBarShowLabel: false,
-              tabBarBadgeStyle: { backgroundColor: 'red' },
-            })}
-          >
-            <Tab.Screen
-              name="Information"
-              component={Information}
-              options={{ title: 'Information', headerShown: false }}
-            />
-            <Tab.Screen
-              name="Tomes"
-              component={TomesStackScreen}
-              options={{ title: 'Tomes', headerShown: false }}
-              listeners={{
-                tabLongPress: () => handleLongPress('tomes'),
-              }}
-            />
-            <Tab.Screen
-              name="Home"
-              component={Home}
-              options={{ title: 'Home', headerShown: false }}
-            />
-            <Tab.Screen
-              name="Lore"
-              component={LoreStackScreen}
-              options={{ title: 'Lore', headerShown: false }}
-              listeners={{
-                tabLongPress: () => handleLongPress('lore'),
-              }}
-            />
-            <Tab.Screen
-              name="User"
-              component={UserProfile}
-              options={{ title: 'User', headerShown: false, tabBarBadge: 1 }}
-            />
-          </Tab.Navigator>
-        ) : (
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Logged out"
-              component={Login}
-              options={{
-                title: 'User logged out - login form',
-                headerShown: false,
-              }}
-            />
-          </Stack.Navigator>
-        )}
-      </NavigationContainer>
+                  if (route.name === 'Home') {
+                    iconName = focused ? 'home' : 'home-outline'
+                  } else if (route.name === 'Tomes') {
+                    iconName = focused ? 'book' : 'book-outline'
+                  } else if (route.name === 'User') {
+                    iconName = focused ? 'person' : 'person-outline'
+                  } else if (route.name === 'Information') {
+                    iconName = focused
+                      ? 'information-circle'
+                      : 'information-circle-outline'
+                  } else if (route.name === 'Lore') {
+                    iconName = focused ? 'add-circle' : 'add'
+                  }
+                  return <Ionicons name={iconName} size={30} color={color} /> // consider icon size
+                },
+                tabBarActiveTintColor: '#171D0B',
+                tabBarInactiveTintColor: 'white',
+                tabBarStyle: styles.bottomTabStyle,
+                tabBarShowLabel: false,
+                tabBarBadgeStyle: { backgroundColor: 'red' },
+              })}
+            >
+              <Tab.Screen
+                name="Information"
+                component={Information}
+                options={{ title: 'Information', headerShown: false }}
+              />
+              <Tab.Screen
+                name="Tomes"
+                component={TomesStackScreen}
+                options={{ title: 'Tomes', headerShown: false }}
+                listeners={{
+                  tabLongPress: () => handleLongPress('tomes'),
+                }}
+              />
+              <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{ title: 'Home', headerShown: false }}
+              />
+              <Tab.Screen
+                name="Lore"
+                component={LoreStackScreen}
+                options={{ title: 'Lore', headerShown: false }}
+                listeners={{
+                  tabLongPress: () => handleLongPress('lore'),
+                }}
+              />
+              <Tab.Screen
+                name="User"
+                component={UserProfile}
+                options={{ title: 'User', headerShown: false, tabBarBadge: 1 }}
+              />
+            </Tab.Navigator>
+          ) : (
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Logged out"
+                component={Login}
+                options={{
+                  title: 'User logged out - login form',
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          )}
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   )
 }
