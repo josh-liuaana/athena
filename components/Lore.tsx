65206355b913ba175528c40b4741cf42389057d5
Josh Liuaana <jfliuaana@gmail.com>
2024-02-05 20:28:38 +0000

    Update lore styling and dispatch

--- components/Lore.tsx
+++ components/Lore.tsx
@@ -1,44 +1,37 @@
 import { View, Text, StyleSheet, Pressable, Alert, Image } from 'react-native'
+
+import appLogo from '../assets/images/athena-favicon-color.png'
 import Icon from 'react-native-vector-icons/Octicons'
-import { useAppDispatch } from '../hooks/redux'
-import { useEffect } from 'react'
-import { fetchThunkBooks } from '../redux/books/booksSlice'
 
 export default function Lore({ navigation }) {
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    dispatch(fetchThunkBooks())
-  }, [])
-
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../assets/images/athena-favicon-color.png')}
-      />
+      <Image style={styles.logo} source={appLogo} />
       <Text style={styles.title}>Add Lore</Text>
+
       <View style={styles.buttonContainer}>
         <View style={styles.topButtonContainer}>
           <Pressable
             style={[styles.button, styles.buttonTop]}
             onPress={() => navigation.navigate('AddCharacter')}
           >
-            <Icon style={styles.icon} name="person" size={60} />
+            <Icon style={styles.icon} name="person" size={47} />
           </Pressable>
         </View>
+
         <View style={styles.bottomButtonsContainer}>
           <Pressable
             style={[styles.button, styles.buttonLeft]}
             onPress={() => navigation.navigate('AddBook')}
           >
-            <Icon style={styles.icon} name="book" size={60} />
+            <Icon style={styles.icon} name="book" size={47} />
           </Pressable>
 
           <Pressable
             style={[styles.button, styles.buttonRight]}
             onPress={() => Alert.alert('Add Location functionality to come')}
           >
-            <Icon style={styles.icon} name="location" size={60} />
+            <Icon style={styles.icon} name="location" size={47} />
           </Pressable>
         </View>
       </View>
@@ -53,22 +46,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo: {
-    height: 75,
-    width: 75,
-    margin: 15,
-  },
-  title: {
-    fontFamily: 'caveat',
-    fontSize: 70,
-    width: '100%',
-    textAlign: 'center',
-  },
-
-  buttonContainer: {
-    flex: 0.35,
-    width: '80%',
-  },
   topButtonContainer: {
     height: '50%',
   },
@@ -76,20 +53,37 @@ const styles = StyleSheet.create({
     height: '50%',
     flexDirection: 'row',
   },
+  buttonTop: {
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  logo: {
+    height: 75,
+    width: 75,
+    margin: 15,
+  },
+  title: {
+    fontFamily: 'vibes',
+    fontSize: 70,
+    width: '100%',
+    textAlign: 'center',
+    color: '#5a712c',
+  },
+  buttonContainer: {
+    margin: 15,
+    flex: 0.3,
+    width: '80%',
+  },
   button: {
-    justifyContent: 'center',
     flex: 0.5,
+    textAlign: 'center',
     padding: 15,
     margin: 5,
     backgroundColor: '#5a712c',
     elevation: 4,
     shadowColor: '#171D0B',
-  },
-
-  buttonTop: {
-    flex: 1,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
+    justifyContent: 'center',
   },
   buttonLeft: {
     borderBottomLeftRadius: 10,
