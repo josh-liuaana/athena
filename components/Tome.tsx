63a8481a5709b0943774fb386f58265495a445b9
Josh Liuaana <jfliuaana@gmail.com>
2024-02-05 20:21:16 +0000

    Add library and style Tomes

--- components/Tome.tsx
+++ components/Tome.tsx
@@ -1,27 +1,42 @@
-import { View, Text, StyleSheet, Pressable, Image, Alert } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native'
+
+import appLogo from '../assets/images/athena-favicon-color.png'
 
 export default function Tomes({ navigation }) {
+  const handleNavigate = (page) => {
+    navigation.navigate(page)
+  }
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../assets/images/athena-favicon-color.png')}
-      />
+      <Image style={styles.logo} source={appLogo} />
       <Text style={styles.title}>Tomes</Text>
+
       <View style={styles.buttonContainer}>
-        <Pressable
-          style={[styles.button, styles.buttonLeft]}
-          onPress={() => navigation.navigate('Characters')}
-        >
-          <Text style={styles.buttonText}>Characters</Text>
-        </Pressable>
-        <Pressable
-          style={[styles.button, styles.buttonRight]}
-          onPress={() => Alert.alert('Work in progress')}
-          // onPress={() => navigation.navigate('Places')}
-        >
-          <Text style={styles.buttonText}>Locations</Text>
-        </Pressable>
+        <View style={styles.topButtonContainer}>
+          <Pressable
+            style={[styles.button, styles.buttonTop]}
+            onPress={() => handleNavigate('Characters')}
+          >
+            <Text style={styles.buttonText}>Characters</Text>
+          </Pressable>
+        </View>
+
+        <View style={styles.bottomButtonsContainer}>
+          <Pressable
+            style={[styles.button, styles.buttonLeft]}
+            onPress={() => handleNavigate('Books')}
+          >
+            <Text style={styles.buttonText}>Library</Text>
+          </Pressable>
+
+          <Pressable
+            style={[styles.button, styles.buttonRight]}
+            onPress={() => handleNavigate('Places')}
+            disabled
+          >
+            <Text style={styles.buttonText}>Locations</Text>
+          </Pressable>
+        </View>
       </View>
     </View>
   )
@@ -34,6 +49,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  topButtonContainer: {
+    height: '50%',
+  },
+  bottomButtonsContainer: {
+    height: '50%',
+    flexDirection: 'row',
+  },
+  buttonTop: {
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
   logo: {
     height: 75,
     width: 75,
@@ -44,10 +71,12 @@ const styles = StyleSheet.create({
     fontSize: 70,
     width: '100%',
     textAlign: 'center',
+    color: '#5a712c',
   },
   buttonContainer: {
     margin: 15,
-    flexDirection: 'row',
+    flex: 0.3,
+    width: '80%',
   },
   button: {
     flex: 0.5,
@@ -57,13 +86,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#5a712c',
     elevation: 4,
     shadowColor: '#171D0B',
+    justifyContent: 'center',
   },
   buttonLeft: {
-    borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
   },
   buttonRight: {
-    borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
     backgroundColor: 'grey',
   },
