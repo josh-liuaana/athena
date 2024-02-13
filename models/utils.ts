57955cf1cc5e4fea395ed3700a04c1e57d037f37
Josh Liuaana <jfliuaana@gmail.com>
2024-02-13 14:17:07 +0000

    add sort functions to utils

--- models/utils.ts
+++ models/utils.ts
@@ -5,3 +5,48 @@ export function validateEmail(email) {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     )
 }
+
+export function sortChar(sortTypeOrder, currentFilteredCharacters) {
+  let sortedArray
+  if (sortTypeOrder === 'date-asc') {
+    sortedArray = [...currentFilteredCharacters].sort(
+      (a, b) => b.dateAdded - a.dateAdded
+    )
+    return {
+      sortedArray,
+      sortType: 'sort-calendar-ascending',
+    }
+  }
+  if (sortTypeOrder === 'date-desc') {
+    sortedArray = [...currentFilteredCharacters].sort(
+      (a, b) => a.dateAdded - b.dateAdded
+    )
+    return {
+      sortedArray,
+      sortType: 'sort-calendar-descending',
+    }
+  }
+  if (sortTypeOrder === 'alpha-asc') {
+    sortedArray = [...currentFilteredCharacters].sort((a, b) => {
+      if (a.name < b.name) {
+        return -1
+      }
+      if (a.name > b.name) {
+        return 1
+      }
+      return 0
+    })
+    return { sortedArray, sortType: 'sort-alphabetical-ascending' }
+  } else if (sortTypeOrder === 'alpha-desc') {
+    sortedArray = [...currentFilteredCharacters].sort((a, b) => {
+      if (a.name < b.name) {
+        return 1
+      }
+      if (a.name > b.name) {
+        return -1
+      }
+      return 0
+    })
+    return { sortedArray, sortType: 'sort-alphabetical-descending' }
+  }
+}
