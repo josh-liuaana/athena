b423a33fa6897106ae5b6a06357c2975d998ffb9
Josh Liuaana <jfliuaana@gmail.com>
2024-02-19 16:26:00 +0000

    Update user section

--- components/user/EditProfile.tsx
+++ components/user/EditProfile.tsx
@@ -1,22 +1,31 @@
 import { View, Text, StyleSheet } from 'react-native'
 
+import { auth } from '../../firebase.config'
+
 export default function EditProfile() {
-  // name user name
-  // update email
-  // change password
-  // delete account
+  const user = auth.currentUser
 
   return (
     <View style={styles.container}>
-      <Text>EditProfile</Text>
+      <Text style={styles.text}>{user.displayName}</Text>
+      <Text style={styles.text}>{user.email}</Text>
+      <Text style={styles.text}>
+        Member since: {user.metadata.creationTime}
+      </Text>
+      <Text style={styles.text}>
+        Last log in {user.metadata.lastSignInTime}
+      </Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
+  },
+  text: {
+    fontSize: 20,
   },
 })
--- components/user/UserProfile.tsx
+++ components/user/UserProfile.tsx
@@ -1,49 +1,175 @@
-import { View, Text, StyleSheet, Button, Alert, Pressable } from 'react-native'
+import { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Image, Alert } from 'react-native'
 
-import { signOut } from 'firebase/auth'
+import { Banner, Portal, FAB, Icon, Avatar } from 'react-native-paper'
+import IconAwesome from 'react-native-vector-icons/FontAwesome'
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons'
 
-import Icon from 'react-native-vector-icons/AntDesign'
+import ErrorComp from '../Error'
+import EditProfile from './EditProfile'
+
+import { sendEmailVerification, signOut } from 'firebase/auth'
 
 import { auth } from '../../firebase.config'
-import React from 'react'
-import { homeStyles } from '../../styles/styles'
+
+import { useAppDispatch, useAppSelector } from '../../hooks/redux'
+
+import { showError } from '../../redux/error/errorSlice'
+
+import { createBookArray, mostCommonElement } from '../../models/utils'
 
 export default function UserProfile() {
+  const dispatch = useAppDispatch()
+  const error = useAppSelector((state) => state.error)
+  const books = useAppSelector((state) => state.books)
+  const characters = useAppSelector((state) => state.characters)
+
+  const [bannerVisible, setBannerVisible] = useState<boolean>()
+  const [verificationPending, setVerificationPending] = useState(false)
+  const [fabOpen, setFabOpen] = useState(false)
+  const [bookCount, setBookCount] = useState({}) as any
+  const [editing, setEditing] = useState(false)
+
   const user = auth.currentUser
+
+  useEffect(() => {
+    setBannerVisible(!user?.emailVerified)
+    const bookArr = createBookArray(characters.characterList)
+    const highestBookCount = mostCommonElement(bookArr)
+    setBookCount(highestBookCount)
+  }, [])
 
   const handleLogout = async () => {
     try {
       await signOut(auth)
     } catch (err) {
-      throw new Error(err.message)
+      dispatch(
+        showError({
+          errorTechnical: err.message,
+          errorMessage: 'Could not handle the log out, try again later',
+        })
+      )
+    }
+  }
+
+  const handleResendVerificationEmail = () => {
+    try {
+      sendEmailVerification(auth.currentUser)
+      setBannerVisible(false)
+      setVerificationPending(true)
+    } catch (err) {
+      dispatch(
+        showError({
+          errorTechnical: err.message,
+          errorMessage:
+            'Something went wrong with you email verification, we are looking into it',
+        })
+      )
     }
   }
 
   return (
     <View style={styles.container}>
-      <Text>Hello {user.displayName}</Text>
-      <Text>{user.email}</Text>
-      {!user?.emailVerified && (
-        <View>
-          <Text style={{ color: 'red' }}>
-            Email address not yet verified, please verify
+      {error && <ErrorComp />}
+      <Portal>
+        <Banner
+          visible={bannerVisible}
+          actions={[
+            {
+              label: 'Close',
+              onPress: () => setBannerVisible(false),
+            },
+            {
+              label: 'Resend Verification Email',
+              onPress: () => handleResendVerificationEmail(),
+            },
+          ]}
+          icon="gmail"
+          style={{ paddingTop: 25 }}
+        >
+          Your Email account still hasn't been verified. Verify before you get
+          locked out
+        </Banner>
+      </Portal>
+
+      <Avatar.Text label={user.displayName.slice(0, 1)} />
+
+      <Text style={styles.nameTitle}>{user.displayName}</Text>
+
+      {editing ? (
+        <EditProfile />
+      ) : (
+        <>
+          <Text style={styles.information}>Your Lore count:</Text>
+          <View style={styles.iconContainer}>
+            <View>
+              <IconAwesome style={styles.icon} name="book" size={47} />
+              <Text style={styles.iconText}>{books.bookList.length}</Text>
+            </View>
+            <View>
+              <IconMaterial style={styles.icon} name="human" size={47} />
+              <Text style={styles.iconText}>
+                {characters.characterList.length}
+              </Text>
+            </View>
+            <View>
+              <IconMaterial style={styles.icon} name="city" size={47} />
+              <Text style={styles.iconText}>0</Text>
+            </View>
+          </View>
+
+          <Text style={styles.category}>Current book:</Text>
+          <Image style={styles.image} source={{ uri: books.current.cover }} />
+          <Text style={styles.information}>
+            {' '}
+            {books.current.title} - {books.current.author}
           </Text>
-          <Button
-            title="Resend verification email"
-            onPress={() => Alert.alert('not quite there yet')}
-          />
-        </View>
+          <Text style={styles.category}>Latest character:</Text>
+          <Text style={styles.information}>
+            {characters.characterList[0].name} -{' '}
+            {characters.characterList[0].books[0]}
+          </Text>
+          <Text style={styles.category}>Book with the most characters: </Text>
+          <Text style={styles.information}>
+            {bookCount.highestBookCount} - {bookCount.max}
+          </Text>
+          {!user?.emailVerified && verificationPending && (
+            <View>
+              <Text> Verification pending </Text>
+            </View>
+          )}
+        </>
       )}
 
-      <Button
-        title="Update User Information"
-        onPress={() => Alert.alert('User update form')}
+      <FAB.Group
+        open={fabOpen}
+        visible
+        icon="menu"
+        onStateChange={() => setFabOpen(!fabOpen)}
+        rippleColor="#5a712c"
+        actions={[
+          {
+            icon: 'delete',
+            label: `Delete`,
+            onPress: () => Alert.alert("Hah nope, you're not leaving"),
+          },
+          {
+            icon: 'account-edit',
+            label: `User Information`,
+            onPress: () => setEditing(true),
+          },
+          {
+            icon: 'human',
+            label: `Profile`,
+            onPress: () => setEditing(false),
+          },
+          {
+            icon: 'logout',
+            label: `Log out`,
+            onPress: () => handleLogout(),
+          },
+        ]}
       />
-      <View>
-        <Pressable onPress={handleLogout}>
-          <Icon style={homeStyles.icon} name="logout" />
-        </Pressable>
-      </View>
     </View>
   )
 }
@@ -53,5 +179,48 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#dbe2cc',
+    gap: 15,
+  },
+  logo: {
+    height: 100,
+    width: 101,
+    margin: 15,
+  },
+  logoEditing: {
+    height: 75,
+    width: 75,
+  },
+  nameTitle: {
+    fontFamily: 'caveat',
+    fontSize: 50,
+    width: '80%',
+    textAlign: 'center',
+  },
+  category: {
+    fontFamily: 'caveat',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  information: {
+    fontFamily: 'sans-serif',
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#5a712c',
+  },
+  image: {
+    height: 150,
+    width: 90,
+  },
+  icon: {
+    color: '#5a712c',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    gap: 30,
+  },
+  iconText: {
+    fontSize: 30,
+    textAlign: 'center',
   },
 })
